400bd384adbb6fd904ec4b8082c695bda138f834
Daniil Maksymenko <konor00792@gmail.com>
2020-02-10 16:08:28 +0200

    Update run_ner.sh

--- run_ner.sh
+++ run_ner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-  python ./NER/BERT_NER.py\
+  python BERT_NER.py\
     --task_name="NER"  \
     --do_lower_case=False \
     --crf=True \
@@ -18,4 +18,4 @@
     --output_dir=./output
 
 
-perl ./NER/conlleval.pl -d '\t' < ./output/labels_test.txt
+perl conlleval.pl -d '\t' < ./output/result_dir/labels_test.txt
